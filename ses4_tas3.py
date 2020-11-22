514244644662c99d6fe8ab0e9d1141e72cab456a
E17538 <erfan.ebrahimi@uniper.energy>
2020-11-22 19:13:21 +0100

    Denoising an image by keeping the borders

---
+++ ses4_tas3.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2 as cv
+
+
+img_path = r'C:\Users\E17538\OneDrive - Uniper SE\Desktop\DailyActivities\FAD\Sol_ses3\base.png'
+img = cv.imread(img_path, 1)
+cv.imshow('base image', img)
+
+# apply bilateral filter to smooth texture
+d = 11
+bilateral_img = cv.bilateralFilter(img, d, 75, 75)
+cv.imshow('bilateral filter on original img', bilateral_img)
+
+
+# apply median rank to bilateral output to denoise more but keep the border
+ksize = 1
+median_bilat_img = cv.medianBlur(bilateral_img, ksize=ksize)
+cv.imshow('Median blur on bilat', median_bilat_img)
+
+
+cv.waitKey(delay=0) == '27'
+cv.destroyAllWindows()
